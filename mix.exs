60d361738766a08d49664cb7b35bfa3d6aa8e4b7
Samuel Ventura <samuel.ventura@yeico.com>
2017-09-22 20:38:34 -0700

    Updated to sniff 0.1.1 which was confirmed to run on a clean Windows10 VM with erl9.0/OTP20/Elixir1.4.5.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Baud.Mixfile do
 
   def project do
     [app: :baud,
-     version: "0.5.0",
+     version: "0.5.1",
      elixir: "~> 1.3",
      compilers: Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -20,7 +20,7 @@ defmodule Baud.Mixfile do
 
   defp deps do
     [
-      {:sniff, "~> 0.1.0"},
+      {:sniff, "~> 0.1.1"},
       {:modbus, "~> 0.3.4"},
       {:ex_doc, "~> 0.12", only: :dev},
     ]
